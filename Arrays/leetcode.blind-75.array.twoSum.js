fc402a44b0fca9c7775c32373f82576c375f35ad
Sergei Liddell <liddell.s@pm.me>
2021-02-23 14:16:56 -0700

    Improves twoSum() and offers Improvments/Analysis

--- Arrays/leetcode.blind-75.array.twoSum.js
+++ Arrays/leetcode.blind-75.array.twoSum.js
@@ -34,5 +34,15 @@ const twoSum = function (arr, target) {
     }
   }
 }
+// time: o(n)
+// space: 0(1)
+
+
+// Improve space complexity would mean to first sort the input array.
+// Best sorting algorthm for a worst case would b O (n log n)? So where
+// space is needed we can implement sort. From there, start pointers on either end.
+// The logic is that we need to ask does start + end === result? no. move either the start or end based upon if target is smaller than the calculation, increase start, else target is larger so move end pointer to the left to become smaller. stop when start >= end.
+// this will result in O(n log n) for time and 0(1) for space
+
 
 console.log(twoSum([2, 7, 11, 15], 9)) // -> [0, 1]
