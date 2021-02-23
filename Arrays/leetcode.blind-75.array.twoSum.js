e86c52a614ae880ec946e0b972c61fcd00a8a1b5
Sergei Liddell <liddell.s@pm.me>
2021-02-23 14:06:15 -0700

    Solves twoSum()
    * Uses a Hashtable

---
+++ Arrays/leetcode.blind-75.array.twoSum.js
@@ -0,0 +1,38 @@
+/*
+https://leetcode.com/problems/two-sum/
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*/
+
+const twoSum = function (arr, target) {
+  const numbersSeen = {};
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i]
+    let opposite = target - current
+    if (numbersSeen[opposite] >= 0) {
+      return [numbersSeen[opposite], i]
+    } else {
+      numbersSeen[current] = i
+    }
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)) // -> [0, 1]
