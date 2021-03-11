42149d020c87a4f5c82f15885d4525061e2962ae
Sergei Liddell <liddell.s@pm.me>
2021-03-10 20:29:25 -0700

    Enables colors when printing Nodes

--- LinkedLists/ctci.linkedlist.removeDupes.js
+++ LinkedLists/ctci.linkedlist.removeDupes.js
@@ -5,8 +5,7 @@ const list = new LinkedList
 list.addAtHead(300)
 list.addAtHead(200)
 list.addAtHead(100)
-// console.log(list)
-console.log(util.inspect(list, { showHidden: false, depth: null }))
+console.log(util.inspect(list, false, null, true /* enable colors */))
 
 
 // Remove Dupes (2.1)
