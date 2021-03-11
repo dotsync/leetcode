1ea4fad11544233816fb4a1eee94e7dc2291e78c
Sergei Liddell <liddell.s@pm.me>
2021-03-10 20:27:38 -0700

    Imports UTIL to show entire object in console

--- LinkedLists/ctci.linkedlist.removeDupes.js
+++ LinkedLists/ctci.linkedlist.removeDupes.js
@@ -1,8 +1,15 @@
-const Node = require('./Node');
-const arrayTester = require('../arrayTester')
+const util = require('util')
+const LinkedList = require('./LinkedList');
+
+const list = new LinkedList
+list.addAtHead(300)
+list.addAtHead(200)
+list.addAtHead(100)
+// console.log(list)
+console.log(util.inspect(list, { showHidden: false, depth: null }))
+
+
 // Remove Dupes (2.1)
 
 // Time
 // Space
-
-arrayTester(LinkedList.removeDupes([2, 1, 3, 2, 9, 1, 1]), [2, 1, 3, 9]);
