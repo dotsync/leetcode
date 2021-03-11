eb02f8aefb5128a3ad7387ab40e6323570b7c566
Sergei Liddell <liddell.s@pm.me>
2021-03-10 20:45:53 -0700

    Creates 5 tests for LinkedList DS

---
+++ LinkedLists/LinkedList.test.js
@@ -0,0 +1,38 @@
+const LinkedList = require('./LinkedList');
+
+describe('Linked List', () => {
+  describe('#addAtHead', () => {
+    test('should be an empty list', () => {
+      const list = new LinkedList;
+      expect(list.length).toBe(0);
+    })
+    test('should have no head', () => {
+      const list = new LinkedList;
+      expect(list.head).toBe(null);
+    })
+    // add first item to list at head
+    test('should add data to begining of list when list is empty', () => {
+      const list = new LinkedList;
+      list.addAtHead('first in')
+      expect(list.head.data).toBe('first in');
+    })
+    // add two more items to list at head
+    test('should have the most recently added item at the head', () => {
+      const list = new LinkedList;
+      list.addAtHead('first in')
+      list.addAtHead('second in')
+      list.addAtHead('third in')
+      expect(list.head.data).toBe('third in');
+    })
+    test('should have a length of 3', () => {
+      const list = new LinkedList;
+      list.addAtHead('first in')
+      list.addAtHead('second in')
+      list.addAtHead('third in')
+      expect(list.length).toBe(3);
+    })
+  })
+  describe('addAtTail', () => {
+
+  })
+})
