fde4f8b4bb3860e7a7fc73751e8c7554ea63096d
Sergei Liddell <liddell.s@pm.me>
2021-03-10 19:58:21 -0700

    Creates LinkedList.addAtHead() method

--- LinkedLists/LinkedList.js
+++ LinkedLists/LinkedList.js
@@ -1,13 +1,30 @@
+const Node = require('./Node');
+
 class LinkedList {
   constructor() {
     this.head = null;
+    this.length = 0;
   }
 
   // find
   // addAtHead
+  addAtHead(data) {
+    const oldHead = this.head
+    // check if there is a head
+    if (oldHead !== null) {
+      // yes, rearange poiinters
+      this.head = new Node(data)
+      this.head.setNextNode(oldHead)
+    } else { // else head = created head
+      this.head = new Node(data);
+    }
+    this.length++;
+  }
   // addAtTail
   // addAtIndex
   // deleteAtIndex
   // print
 
 }
+
+module.exports = LinkedList;
