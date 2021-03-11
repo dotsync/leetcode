0e92a2e72170d211bbb1a60a13c49c45e829b067
Sergei Liddell <liddell.s@pm.me>
2021-03-11 10:50:46 -0700

    Designs basic workflow for custom debugging

--- LinkedLists/ctci.linkedlist.removeDupes.js
+++ LinkedLists/ctci.linkedlist.removeDupes.js
@@ -1,10 +1,16 @@
 const LinkedList = require('./LinkedList');
+// Custom Debugging Scenario
 // Remove Dupes (2.1)
 
-const duplicates = LinkedList.createListFromValues(10, 10, 20, 30, 40)
-duplicates.printList(true)
-
-// Remove Dupes (2.1)
-
+// Sets up the initial problem with a custom test case
+const customTestCase = [10, 10, 20, 30, 40]
+const duplicates = LinkedList.createListFromValues(...customTestCase)
+// Prints the unsolved problem
+duplicates.printList()
+// Applies the method to the custom test case
+duplicates.removeDupes();
+// Prints the solved Problem
+console.log('\n\n\n\n\n\nSolution:')
+duplicates.printList();
 // Time
 // Space
