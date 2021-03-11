a4f74fcb70e9a42d8e7a8785e35f550c77387d76
Sergei Liddell <liddell.s@pm.me>
2021-03-10 20:08:58 -0700

    Implements LinkedList.findNodeInList() method

--- LinkedLists/LinkedList.js
+++ LinkedLists/LinkedList.js
@@ -6,18 +6,33 @@ class LinkedList {
     this.length = 0;
   }
 
-  // find
+  // findNodeInList
+  findNodeInList(data) {
+    // iterate list
+    let currentNode = this.head
+    let counter = 0;
+    while (counter <= this.length) {
+      // check if current node.data === data
+      if (currentNode.data === data) {
+        // return the node
+        return currentNode.data;
+      }
+      // update current to be curr.next
+      currentNode = currentNode.getNextNode();
+      // counter ++
+      counter++;
+    }
+    // not found
+    return undefined;
+  }
+
   // addAtHead
   addAtHead(data) {
-    const oldHead = this.head
+    const newHead = new Node(data);
+    const oldHead = this.head;
     // check if there is a head
-    if (oldHead !== null) {
-      // yes, rearange poiinters
-      this.head = new Node(data)
-      this.head.setNextNode(oldHead)
-    } else { // else head = created head
-      this.head = new Node(data);
-    }
+    if (oldHead !== null) this.head.setNextNode(oldHead);
+    else this.head = newHead;
     this.length++;
   }
   // addAtTail
