72e0d4eb8228030e1fa978ff2f27834547c2ae6d
Sergei Liddell <liddell.s@pm.me>
2021-03-10 19:17:47 -0700

    Removes test code for Node class

--- LinkedLists/ctci.linkedlist.removeDupes.js
+++ LinkedLists/ctci.linkedlist.removeDupes.js
@@ -1,12 +1,5 @@
-// Remove Dupes (2.1)
 const Node = require('./Node');
-
-const firstNode = new Node('I am the first Node');
-const secondNode = new Node('I am the second Node');
-firstNode.setNextNode(secondNode);
-
-console.log(firstNode);
-// console.log(firstNode.getNextNode());
+// Remove Dupes (2.1)
 
 const removeDupes = (head) => {
 
