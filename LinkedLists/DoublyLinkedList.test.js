589cc781b2490600c0cd727285291c4ea7b8ed1a
Sergei Liddell <liddell.s@pm.me>
2021-03-14 08:47:35 -0600

    Creates test, #addToHead on empty DoublyLinkedList

--- LinkedLists/DoublyLinkedList.test.js
+++ LinkedLists/DoublyLinkedList.test.js
@@ -9,11 +9,31 @@ describe('Doubly Linked List', () => {
     expect(doublyLinkedList.tail).toBeNull();
   })
   // methods
-  // #addToHead
-  // #addToTail
-  // #addAtIndex
-  // #findSpecificIndex
-  // #findSpecificData
-  // #printList
+  describe('Doubly Linked List Methods', () => {
+    // #addToHead
+    describe('#addToHead', () => {
+      // adding when list is empty
+      test('should add new node to empty list', () => {
+        const doublyLinkedList = new DoublyLinkedList;
+        doublyLinkedList.addToHead(1);
+        expect(doublyLinkedList.head.data).toBe(1);
+        expect(doublyLinkedList.head.next).toStrictEqual(doublyLinkedList.tail);
+        expect(doublyLinkedList.head.prev).toBeNull();
+        expect(doublyLinkedList.tail.next).toBeNull();
+        expect(doublyLinkedList.tail.prev).toStrictEqual(doublyLinkedList.head);
+        expect(doublyLinkedList.length).toBe(1);
+      })
+
+      // adding when list is not empty
+    })
+    // #addToTail
+    describe('#addToTail', () => {
+
+    })
+    // #addAtIndex
+    // #findSpecificIndex
+    // #findSpecificData
+    // #printList
+  })
 })
 
