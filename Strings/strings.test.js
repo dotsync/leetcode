3abd63a2280073b985819202bc1e614fcda8dc4d
Sergei Liddell <liddell.s@pm.me>
2021-03-14 11:59:46 -0600

    Implements jest test for stringCompression

--- Strings/strings.test.js
+++ Strings/strings.test.js
@@ -1,5 +1,6 @@
 const isOneAway = require('./ctci.strings.isOneAway');
 const uRLify = require('./ctci.strings.uRLify');
+const stringCompression = require('./ctci.strings.stringCompression')
 
 describe('Strings', () => {
   describe('Cracking The Coding Interview', () => {
@@ -15,6 +16,14 @@ describe('Strings', () => {
     describe('uRLify()', () => {
       test('should replace spaces with %20', () => {
         expect(uRLify('Mr John Smith    ', 13)).toBe('Mr%20John%20Smith')
+      })
+    })
+    describe('stringCompression', () => {
+      test('should compress the string meaning all repeats are removed and indicated', () => {
+        expect(stringCompression('aabcccccaa')).toBe('a2b1c5a2')
+      })
+      test('should not compress because all letters are different ', () => {
+        expect(stringCompression('abc')).toBe('abc')
       })
     })
   })
