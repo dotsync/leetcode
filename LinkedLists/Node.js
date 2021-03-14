b868b410f22bff98b2540110364e91f43cffeabb
Sergei Liddell <liddell.s@pm.me>
2021-03-14 08:26:42 -0600

    Updates Node Class to have prev property / methods

--- LinkedLists/Node.js
+++ LinkedLists/Node.js
@@ -1,16 +1,23 @@
 class Node {
   constructor(data) {
-    this.data = data
-    this.next = null
+    this.data = data;
+    this.next = null;
+    this.prev = null;
   }
 
   getNextNode() {
     return this.next;
   }
-
+  getPreviousNode() {
+    return this.prev;
+  }
   setNextNode(node) {
     if (node instanceof Node || node === null) this.next = node;
-    else throw new Error('Unable to use method setNextNode because the input is not a node or is not null');
+    else throw new Error('Unable to use method setNextNode because the input is not a node instance or the node is not null');
+  }
+  setPreviousNode() {
+    if (node instanceof Node || node === null) this.prev = node;
+    else throw new Error('Unable to use method setPreviousNoce because the input is not a node instance or the node is not null')
   }
 
 }
