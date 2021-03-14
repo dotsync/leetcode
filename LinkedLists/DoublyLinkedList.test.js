db52b30316da4ab012d77e8fe27f537982348fee
Sergei Liddell <liddell.s@pm.me>
2021-03-14 08:35:08 -0600

    Creates test for empty DOuble linked list

---
+++ LinkedLists/DoublyLinkedList.test.js
@@ -0,0 +1,12 @@
+const DoublyLinkedList = require('./DoublyLinkedList');
+
+
+describe('Doubly Linked List', () => {
+  test('should be instantiated with a head, tail and length', () => {
+    const doublyLinkedList = new DoublyLinkedList;
+    expect(doublyLinkedList.length).toBe(0);
+    expect(doublyLinkedList.head).toBeNull();
+    expect(doublyLinkedList.tail).toBeNull();
+  })
+  // methods
+})
