95122ef653418b5e5789694c07928c42d81dfd03
Sergei Liddell <liddell.s@pm.me>
2021-03-15 05:35:23 -0600

    Creates test suite for DoublyLinkedList #addToTail

--- LinkedLists/DoublyLinkedList/DoublyLinkedList.test.js
+++ LinkedLists/DoublyLinkedList/DoublyLinkedList.test.js
@@ -10,7 +10,6 @@ describe('Doubly Linked List', () => {
   })
   // methods
   describe('Doubly Linked List Methods', () => {
-    // #addToHead
     describe('#addToHead', () => {
       // adding when list is empty
       test('should add new head to empty list', () => {
@@ -47,9 +46,31 @@ describe('Doubly Linked List', () => {
         expect(doublyLinkedList.tail.prev.data).toBe(9)
       })
     })
-    // #addToTail
     describe('#addToTail', () => {
-
+      test('should add to tail on empty list', () => {
+        const doublyLinkedList = new DoublyLinkedList;
+        doublyLinkedList.addToTail(1);
+        expect(doublyLinkedList.tail.data).toBe(1);
+        expect(doublyLinkedList.tail.next).toBeNull();
+        expect(doublyLinkedList.tail.prev.prev).toBeNull();
+        // when adding just one item, and tail is empty. tail and head represent item
+        expect(doublyLinkedList.tail.prev.data).toBe(1);
+        expect(doublyLinkedList.length).toBe(1);
+      })
+      test('should add to tail when list is not empty', () => {
+        const doublyLinkedList = new DoublyLinkedList
+        doublyLinkedList.addToTail(1)
+        doublyLinkedList.addToTail(2)
+        doublyLinkedList.addToTail(3)
+        // [head = 1] <-> [2] <-> [tail = 3]
+        expect(doublyLinkedList.head.data).toBe(1);
+        expect(doublyLinkedList.head.prev).toBeNull();
+        expect(doublyLinkedList.head.next.data).toBe(2);
+        expect(doublyLinkedList.head.next.next.data).toBe(3);
+        expect(doublyLinkedList.tail.data).toBe(3);
+        expect(doublyLinkedList.tail.next).toBeNull();
+        expect(doublyLinkedList.length).toBe(3);
+      })
     })
     // #addAtIndex
     // #findSpecificIndex
