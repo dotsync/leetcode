73dd6d55c94a6bf53daefff1ad73ff2141984281
Sergei Liddell <liddell.s@pm.me>
2021-03-15 05:23:58 -0600

    solves productOfArrayExceptSelf() problem

--- Arrays/productOfArrayExceptSelf.js
+++ Arrays/productOfArrayExceptSelf.js
@@ -1,17 +1,15 @@
 const productOfArrayExceptSelf = (arr) => {
-
-  for (let i = 0; i < arr.length; i++) {
-    let multiplier = 1;
-    let rp = arr.length - 1;
-    let count = 1;
-    while (rp !== i) {
-      count *= arr[rp];
-      rp--;
-    }
-    multiplier *= arr[i];
-    arr[i] = count;
+  let result = []
+  result[0] = 1;
+  for (let i = 1; i < arr.length; i++) {
+    result[i] = arr[i - 1] * result[i - 1];
   }
-  return arr;
+  let right = 1;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    result[i] = result[i] * right;
+    right *= arr[i];
+  }
+  return result;
 }
 
 module.exports = productOfArrayExceptSelf;
