66a2b99e652291c531dfc3485da2be26d2eb312a
Sergei Liddell <liddell.s@pm.me>
2021-03-17 08:26:52 -0600

    Modifies array to allow random filler

--- Matrix/Matrix.js
+++ Matrix/Matrix.js
@@ -5,9 +5,12 @@ class Matrix {
   // createBoard
   createBoard(cols, rows, filler) {
     // [0, 0, 0]
+    let random = false
+    if (filler === undefined) random = true
     for (let j = 0; j < rows; j++) {
       const row = [];
       for (let i = 0; i < cols; i++) {
+        if (random) filler = Math.floor(Math.random() * 100)
         row.push(filler)
       }
       this.board.push(row)
