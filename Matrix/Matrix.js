0a8b5fd03480bad46fbca1f30666c6e42881ca64
Sergei Liddell <liddell.s@pm.me>
2021-03-18 05:22:26 -0600

    Solves rotting Oranges problem

--- Matrix/Matrix.js
+++ Matrix/Matrix.js
@@ -1,8 +1,16 @@
+/*
+## Matrix
+- [ ] [prison-cells-after-n-days](https://leetcode.com/problems/prison-cells-after-n-days/)
+- [ ] [path-with-maximum-minimum-value](https://leetcode.com/problems/path-with-maximum-minimum-value/)
+- [ ] [spiral-matrix-ii](https://leetcode.com/problems/spiral-matrix-ii/)
+- [] [rotting-oranges](https://leetcode.com/problems/rotting-oranges/)
+- [ ] [walls-and-gates](https://leetcode.com/problems/walls-and-gates/) */
+
 class Matrix {
   constructor() {
     this.board = [];
   }
-  // createBoard
+  // #createBoard
   createBoard(cols, rows, filler) {
     // [0, 0, 0]
     let random = false
@@ -18,7 +26,53 @@ class Matrix {
     }
     return this.board;
   }
-  // transpose
+  // #rotting-oranges
+  rottingOranges(matrix) {
+    const row = matrix.length;
+    const col = matrix[0].length;
+    let fresh = 0;
+    const queue = [];
+    // iterate through matrix row by row
+    for (let i = 0; i < row; i++) {
+      for (let j = 0; j < col; j++) {
+        // if rotten, push to queue
+        if (matrix[i][j] === 2) queue.push([i, j]);
+        // if apple fresh++
+        if (matrix[i][j] === 1) fresh++;
+      }
+    }
+    let minute = 0;
+    // BFS
+    while (queue.length) {
+      const size = queue.length;
+      for (let i = 0; i < size; i++) {
+        const [x, y] = queue.shift();
+        if (x - 1 >= 0 && matrix[x - 1][y] === 1) {
+          matrix[x - 1][y] = 2;
+          fresh--;
+          queue.push([x - 1, y]);
+        }
+        if (x + 1 < row && matrix[x + 1][y] === 1) {
+          matrix[x + 1][y] = 2;
+          fresh--;
+          queue.push([x + 1, y]);
+        }
+        if (y - 1 >= 0 && matrix[x][y - 1] === 1) {
+          matrix[x][y - 1] = 2;
+          fresh--;
+          queue.push([x, y - 1]);
+        }
+        if (y + 1 < col && matrix[x][y + 1] === 1) {
+          matrix[x][y + 1] = 2;
+          fresh--;
+          queue.push([x, y + 1]);
+        }
+      }
+      if (queue.length > 0) minute++;
+    }
+    // if fresh === 0 return minutes it took, else return -1
+    return fresh === 0 ? minute : -1;
+  }
 }
 
 module.exports = Matrix
--- Matrix/Matrix.test.js
+++ Matrix/Matrix.test.js
@@ -2,25 +2,44 @@ const Matrix = require('./Matrix');
 
 describe('Matrix', () => {
   describe('Matrix Methods', () => {
-    test('should create a 3 x 3 Matrix', () => {
-      let matrix = new Matrix;
-      expect(matrix.createBoard(3, 3, 0)).toStrictEqual([
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]])
+    describe('#createBoard', () => {
+      test('should create a 3 x 3 Matrix', () => {
+        let matrix = new Matrix;
+        expect(matrix.createBoard(3, 3, 0)).toStrictEqual([
+          [0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]])
+      })
+      test('should create a 3 x 2 Matrix', () => {
+        let matrix = new Matrix;
+        expect(matrix.createBoard(2, 3, 0)).toStrictEqual([
+          [0, 0],
+          [0, 0],
+          [0, 0]])
+      })
+      test('should create a 2 x 3 Matrix', () => {
+        let matrix = new Matrix;
+        expect(matrix.createBoard(3, 2, 0)).toStrictEqual([
+          [0, 0, 0],
+          [0, 0, 0]])
+      })
     })
-    test('should create a 3 x 2 Matrix', () => {
-      let matrix = new Matrix;
-      expect(matrix.createBoard(2, 3, 0)).toStrictEqual([
-        [0, 0],
-        [0, 0],
-        [0, 0]])
-    })
-    test('should create a 2 x 3 Matrix', () => {
-      let matrix = new Matrix;
-      expect(matrix.createBoard(3, 2, 0)).toStrictEqual([
-        [0, 0, 0],
-        [0, 0, 0]])
+    describe('#rottingOranges', () => {
+      test('should return -1 if not all oranges will rot', () => {
+        const matrix = new Matrix
+        matrix.board = [[2, 1, 1], [0, 1, 1], [1, 0, 1]]
+        expect(matrix.rottingOranges(matrix.board)).toBe(-1);
+      })
+      test('should return Return the minimum number of minutes that must elapse until no cell has a fresh orange', () => {
+        const matrix = new Matrix;
+        matrix.board = [[2, 1, 1], [1, 1, 0], [0, 1, 1]]
+        expect(matrix.rottingOranges(matrix.board)).toBe(4)
+      })
+      test('should return 0 since all of the oranges are already rotten', () => {
+        const matrix = new Matrix;
+        matrix.board = [[0, 2]]
+        expect(matrix.rottingOranges(matrix.board)).toBe(0)
+      })
     })
   })
 })
