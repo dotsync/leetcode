f58d9dcfbad27a342f0ae82ebcd5315052a6adaf
Sergei Liddell <liddell.s@pm.me>
2021-03-18 07:27:07 -0600

    Creates Basic BST constructor class

---
+++ BST/BST.js
@@ -0,0 +1,18 @@
+/* NOTES
+Look if you wanna get your coding to the next level, U must learn trees!
+Really exciting day, Building my tree class from scratch. Yesterday showed me that this is a weak point in my shield of knowledge
+Must understand tree rtraversal, graph traversal
+decsions forking time and space are like connections in a graph
+*/
+const BinarySearchTreeNode = require('./BSTNode');
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  // BST methods
+  // insert(data)
+  // remove(data)
+}
+
+module.exports = BinarySearchTree;
