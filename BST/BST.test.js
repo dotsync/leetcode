9d388bbb8599a9954ba832b8505fb6df6aff6d72
Sergei Liddell <liddell.s@pm.me>
2021-03-18 07:25:54 -0600

    Creates test for an empty tree

---
+++ BST/BST.test.js
@@ -0,0 +1,14 @@
+const BinarySearchTree = require('./BST');
+
+describe('BinarySearchTree', () => {
+  test('should instantiate a root which is equal to null', () => {
+    const tree = new BinarySearchTree;
+    expect(tree.root).toBeNull();
+  })
+
+  describe('BinarySearchTree Methods', () => {
+
+
+  })
+})
+
