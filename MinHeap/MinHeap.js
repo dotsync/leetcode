5a9d7efa62d1ab01f502192021be9a4c6fff1670
Sergei Liddell <liddell.s@pm.me>
2021-03-20 18:31:49 -0600

    Implements MinHeap #bubbleUp method

--- MinHeap/MinHeap.js
+++ MinHeap/MinHeap.js
@@ -7,6 +7,17 @@ class MinHeap {
   add(data) {
     this.heap.push(data);
     this.size++;
+    this.bubbleUp()
+  }
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+  bubbleUp() {
+    let current = this.size
+    while (current > 1 && this.heap[current] < this.heap[Math.floor(current / 2)]) {
+      this.swap(current, Math.floor(current / 2))
+      current = Math.floor(current / 2)
+    }
   }
 }
 
