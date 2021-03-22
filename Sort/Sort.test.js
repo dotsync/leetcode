f094eb8383337206b94ce66bc66d108401774e56
Sergei Liddell <liddell.s@pm.me>
2021-03-22 06:52:32 -0600

    Creates test case for small inputs

--- Sort/Sort.test.js
+++ Sort/Sort.test.js
@@ -8,6 +8,9 @@ describe('Sorting Algorithms', () => {
     test('should sort elements in an array from smallest to largest', () => {
       expect(Sort.bubbleSort([10, 9, 8, 3, 2, 1, 5, 6, 4, 7])).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     })
+    test('should work with inputs of only one value', () => {
+      expect(Sort.bubbleSort([2])).toStrictEqual([2])
+    })
 
   })
 })
