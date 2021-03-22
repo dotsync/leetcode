45cb81628a1f6ecef1fdccb5c0e7f5c154dbe95f
Sergei Liddell <liddell.s@pm.me>
2021-03-22 09:29:36 -0600

    Solves Merge Two Sorted Arrays

--- Sort/Sort.js
+++ Sort/Sort.js
@@ -15,6 +15,36 @@ const bubbleSort = (array) => {
   return copy
 }
 
+const mergeTwoSortedArrays = (arr1, arr2) => {
+  let result = []
+  let i = 0
+  let j = 0
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {
+      result.push(arr1[i])
+      i++
+    } else {
+      result.push(arr2[j])
+      j++
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i])
+    i++
+  }
+  while (j < arr2.length) {
+    result.push(arr1[j])
+    j++
+  }
+  return result
+}
+
+const mergeSort = (arr1, arr2) => {
+
+}
+
 module.exports = {
   bubbleSort,
+  mergeTwoSortedArrays,
+  mergeSort,
 }
