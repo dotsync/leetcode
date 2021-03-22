7352bf3681c50bcf8ad70a1b4ee9c8394d4a00be
Sergei Liddell <liddell.s@pm.me>
2021-03-21 18:08:56 -0600

    Implements #addEdge for Graph class

--- Graph/AdjacencyListGraphUndirected/Graph.js
+++ Graph/AdjacencyListGraphUndirected/Graph.js
@@ -11,18 +11,25 @@ class Graph {
       throw new Error('The vertex you are trying to add already exists. Aborting operation.')
     }
   }
-  addEdge() {
+  addEdge(vertex1, vertex2) {
+    // if vertex doesnt exist, add to list
+    if (!this.adjacencyList[vertex1]) this.adjacencyList[vertex1] = []
+    if (!this.adjacencyList[vertex2]) this.adjacencyList[vertex2] = []
+    // iterate vertex1 array and look for vertex 2, if it doesn;t exist add it,
+    for (let i = 0; i < this.adjacencyList[vertex1]; i++) {
+      if (this.adjacencyList[vertex1[i]] === vertex2) break
+    }
+    this.adjacencyList[vertex1].push(vertex2)
+    // interate vertex2 and look for vertex1
+    for (let i = 0; i < this.adjacencyList[vertex2]; i++) {
+      if (this.adjacencyList[vertex2[i]] === vertex1) break
+    }
+    this.adjacencyList[vertex2].push(vertex1)
   }
   removeEdge() {
   }
   removeVertex() {
   }
 }
-
-const graph = new Graph
-graph.addVertex('hello')
-console.log(graph.adjacencyList)
-graph.addVertex('hello')
-
 
 module.exports = Graph;
