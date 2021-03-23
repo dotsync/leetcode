f0e51402b90b6b002cda229740b62722301fd559
Sergei Liddell <liddell.s@pm.me>
2021-03-23 17:38:47 -0600

    Creates test suite for numIslands problem

---
+++ Graph/graphProblems/graphProblems.test.js
@@ -0,0 +1,27 @@
+const numIslands = require('./numIslands');
+
+describe('numIslands', () => {
+  test('should work with empty inputs', () => {
+    expect(numIslands([])).toBe(0)
+  })
+
+  test('should return the number of islands found in 2d array', () => {
+    const island = [
+      ["1", "1", "1", "1", "0"],
+      ["1", "1", "0", "1", "0"],
+      ["1", "1", "0", "0", "0"],
+      ["0", "0", "0", "0", "0"]
+    ]
+    expect(numIslands(island)).toBe(1)
+  })
+  test('should work for mutliple islands in the input array', () => {
+    const island = [
+      ["1", "1", "0", "0", "0"],
+      ["1", "1", "0", "0", "0"],
+      ["0", "0", "1", "0", "0"],
+      ["0", "0", "0", "1", "1"]
+    ]
+    expect(numIslands(island)).toBe(3)
+  })
+
+})
---
+++ Graph/graphProblems/numIslands.js
@@ -0,0 +1,5 @@
+const numIslands = (matrix) => {
+  if (matrix === null || matrix.length === 0) return 0
+}
+
+module.exports = numIslands
