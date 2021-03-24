69f5b65df0e7730e441cdc97ee4c7ed646403102
Sergei Liddell <liddell.s@pm.me>
2021-03-23 20:40:51 -0600

    Removes console log

--- GraphV2.0/Edge.test.js
+++ GraphV2.0/Edge.test.js
@@ -3,7 +3,7 @@ const Edge = require('./Edge')
 describe('Edge Class', () => {
   test('should have a weight property', () => {
     const edge = new Edge
-    console.log(edge)
+    // console.log(edge)
     expect(edge.weight).toBeNull()
   })
 })
