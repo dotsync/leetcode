8f3a8d6978bb75238c298547bfd7be18ba74f447
Sergei Liddell <liddell.s@pm.me>
2021-03-23 20:11:40 -0600

    Implements Vertex #addEdge and #removeEdge methods

--- GraphV2.0/Vertex.js
+++ GraphV2.0/Vertex.js
@@ -1,6 +1,18 @@
+const Edge = require('./Edge')
+
 class Vertex {
   constructor(data) {
     this.data = data;
     this.edges = [];
+  }
+  addEdge(vertex, weight) {
+    if (vertex instanceof Vertex) {
+      this.edges.push(new Edge(this, vertex, weight))
+    } else {
+      throw new Error('Edge start and end my both be instance of Vertex class')
+    }
+  }
+  removeEdge(vertex) {
+    this.edges = this.edges.filter(e => e.end !== vertex)
   }
 }
