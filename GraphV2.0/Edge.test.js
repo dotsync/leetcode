aa993c5983c95cf988dacbec8664198e2cf9fa22
Sergei Liddell <liddell.s@pm.me>
2021-03-23 20:39:42 -0600

    Creates test case for Edge class

---
+++ GraphV2.0/Edge.test.js
@@ -0,0 +1,10 @@
+const Edge = require('./Edge')
+
+describe('Edge Class', () => {
+  test('should have a weight property', () => {
+    const edge = new Edge
+    console.log(edge)
+    expect(edge.weight).toBeNull()
+  })
+})
+
