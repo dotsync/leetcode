48e0117c6770d402fa26b90c7483da905906ec9c
Sergei Liddell <liddell.s@pm.me>
2021-03-25 19:14:55 -0600

    Fixes bug allowing all sizes and variations

--- Arrays/intersect.js
+++ Arrays/intersect.js
@@ -1,18 +1,28 @@
 const intersect = (nums1, nums2) => {
-  let longerArray = nums1.length > nums2.length ? nums1 : nums2
+  let longArray
+  let shortArray
+  if (nums1.length >= nums2.length) {
+    longArray = nums1
+    shortArray = nums2
+  } else {
+    longArray = nums2
+    shortArray = nums1
+  }
+
   const result = []
   const nums = {}
-  for (let i = 0; i < nums1.length; i++) {
-    if (nums[nums1[i]] == null) {
-      nums[nums1[i]] = 1
+  for (let i = 0; i < longArray.length; i++) {
+    if (nums[longArray[i]] == null) {
+      nums[longArray[i]] = 1
     } else {
-      nums[nums1[i]]++
+      nums[longArray[i]]++
     }
   }
-  for (let i = 0; i < nums2.length; i++) {
-    if (nums[nums2[i]] > 0) {
-      result.push(nums2[i])
-      nums[nums2[i]]--
+
+  for (let i = 0; i < shortArray.length; i++) {
+    if (nums[shortArray[i]] > 0) {
+      result.push(shortArray[i])
+      nums[shortArray[i]]--
     }
   }
 
