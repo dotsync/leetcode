745d71cb7ded0a59f88d52d92b14b7113e4bbc24
Sergei Liddell <liddell.s@pm.me>
2021-03-25 19:19:24 -0600

    Solves singleNumber leetcode problem

---
+++ Arrays/singleNumber.js
@@ -0,0 +1,11 @@
+const singleNumber = nums => {
+  const sorted = nums.sort();
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] !== sorted[i + 1]) {
+      return sorted[i]
+    } else { // does match
+      // should move a total of two spaces
+      i++
+    }
+  }
+};
