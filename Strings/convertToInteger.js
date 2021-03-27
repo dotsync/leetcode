e76bbf85e0afa5402bb8b38310bba8061576ce86
Sergei Liddell <liddell.s@pm.me>
2021-03-26 20:41:46 -0600

    Attempt Atoi problem

---
+++ Strings/convertToInteger.js
@@ -0,0 +1,9 @@
+let string = '1'
+let result = string - '0'
+// result is now type number!
+// So say I want to convert a string to a numbers easily.
+
+let stringNumber = '12345678'
+console.log('stringNumber type', typeof stringNumber)
+stringNumber = stringNumber - '0'
+console.log('stringNumber type', typeof stringNumber)
