e37fc24e72c225a225d34bd54209927cca2a6e53
Sergei Liddell <liddell.s@pm.me>
2021-03-30 14:58:55 -0600

    Fixes bug, input needed to be head not entire list

--- PowerWeekOne/Day02-LinkedList/day02.test.js
+++ PowerWeekOne/Day02-LinkedList/day02.test.js
@@ -11,25 +11,25 @@ describe('Day02 LinkedList List Section', () => {
     test('should work when given two empty lists', () => {
       const list1 = new SinglyLinkedList
       const list2 = new SinglyLinkedList
-      expect(mergeTwoSortedLists(list1, list2)).toBeNull();
+      expect(mergeTwoSortedLists(list1.head, list2.head)).toBeNull();
     })
     test('should merge when list 1 is empty and 2 is full', () => {
       const list1 = new SinglyLinkedList
       const list2 = SinglyLinkedList.createListFromValues(2, 3, 5);
       const expected = SinglyLinkedList.createListFromValues(2, 3, 5);
-      expect(mergeTwoSortedLists(list1, list2)).toStrictEqual(expected);
+      expect(mergeTwoSortedLists(list1.head, list2.head)).toStrictEqual(expected.head);
     })
     test('should merge when list 2 is empty and 1 is full', () => {
       const list1 = SinglyLinkedList.createListFromValues(1, 4, 6);
       const list2 = new SinglyLinkedList
       const expected = SinglyLinkedList.createListFromValues(1, 4, 6);
-      expect(mergeTwoSortedLists(list1, list2)).toStrictEqual(expected);
+      expect(mergeTwoSortedLists(list1.head, list2.head)).toStrictEqual(expected.head);
     })
     test('should merge the two sorted lists', () => {
       const list1 = SinglyLinkedList.createListFromValues(1, 4, 6);
       const list2 = SinglyLinkedList.createListFromValues(2, 3, 5);
       const expected = SinglyLinkedList.createListFromValues(1, 2, 3, 4, 5, 6);
-      expect(mergeTwoSortedLists(list1, list2)).toStrictEqual(expected);
+      expect(mergeTwoSortedLists(list1.head, list2.head)).toStrictEqual(expected.head);
     })
   })
   describe('reverseLinkedList', () => {
