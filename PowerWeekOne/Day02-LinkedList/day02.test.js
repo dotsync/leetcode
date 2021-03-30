e0e373c41d0e68c3d2f197e1ace8568843f4281c
Sergei Liddell <liddell.s@pm.me>
2021-03-30 14:35:02 -0600

    Creates failing test suite for mergeTwoSortedLists

--- PowerWeekOne/Day02-LinkedList/day02.test.js
+++ PowerWeekOne/Day02-LinkedList/day02.test.js
@@ -1,19 +1,25 @@
 const SinglyLinkedList = require('../../LinkedLists/SinglyLinkedList/SinglyLinkedList');
 const copyListWithRandomPointer = require('./copyListWithRandomPointer');
 const reverseLinkedList = require('./reverseLinkedList');
+const mergeTwoSortedLists = require('mergeTwoSortedLists');
 
 describe('Day02 LinkedList List Section', () => {
-  describe('copyListWithRandomPointer', () => {
-
-  })
-  describe('addingTwoNumbers', () => {
-
-  })
-  // describe('mergeKSortedLists', () => {
-
-  // })
+  /********************/
+  // Easy Problems
+  /********************/
   describe('mergeTwoSortedLists', () => {
+    test('should merge when list 1 is empty and 2 is full', () => {
 
+    })
+    test('should merge when list 2 is empty and 1 is full', () => {
+
+    })
+    test('should merge the two sorted lists', () => {
+      const list1 = SinglyLinkedList.createListFromValues(1, 4, 6);
+      const list2 = SinglyLinkedList.createListFromValues(2, 3, 5);
+      const expected = SinglyLinkedList.createListFromValues(1, 2, 3, 4, 5, 6);
+      expect(mergeTwoSortedLists(list1, list2)).toStrictEqual(expected);
+    })
   })
   describe('reverseLinkedList', () => {
     test('should work with an empty list', () => {
@@ -26,7 +32,22 @@ describe('Day02 LinkedList List Section', () => {
       expect(reverseLinkedList(input.head)).toStrictEqual(expected.head)
     })
   })
+  /********************/
+  // Medium Problems
+  /********************/
+  describe('copyListWithRandomPointer', () => {
+
+  })
+  describe('addingTwoNumbers', () => {
+
+  })
+  /********************/
+  // Hard Problems
+  /********************/
   // describe('reverseNodesInKGroups', () => {
+
+  // })
+  // describe('mergeKSortedLists', () => {
 
   // })
 })
