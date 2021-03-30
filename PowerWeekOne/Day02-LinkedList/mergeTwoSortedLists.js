509cafbeb971b95ae2db9e09d529a292a2ea16e4
Sergei Liddell <liddell.s@pm.me>
2021-03-30 15:01:21 -0600

    Solves mergeTwoSortedLists

--- PowerWeekOne/Day02-LinkedList/mergeTwoSortedLists.js
+++ PowerWeekOne/Day02-LinkedList/mergeTwoSortedLists.js
@@ -1,8 +1,28 @@
-const mergeTwoSortedLists = (list1, list2) => {
+const mergeTwoSortedLists = (list1Head, list2Head) => {
+  // create temp head node to attach new list to
   const preHead = {
     data: -1,
     next: null,
   }
+  let current = preHead;
+  // iterate both lists while at least one of the lists still has items
+  while (list1Head != null && list2Head != null) {
+    if (list1Head.data <= list2Head.data) {
+      current.next = list1Head;
+      list1Head = list1Head.next;
+    } else {
+      current.next = list2Head;
+      list2Head = list2Head.next
+    }
+    current = current.next
+  }
+  // if list1 or list2 still has items, append to the end of current
+  if (list1Head != null) {
+    current.next = list1Head;
+  } else if (list2Head != null) {
+    current.next = list2Head;
+  }
+  // return the temp head nodes next property to return full list
   return preHead.next
 }
 
