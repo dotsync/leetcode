5dc795c08b46f89fb0d51d79291b8d592be0b838
Sergei Liddell <liddell.s@pm.me>
2021-03-30 14:00:34 -0600

    Solves containerWithMostWater problem

---
+++ PowerWeekOne/Day01-StringsAndArrays/containerWithMostWater.js
@@ -0,0 +1,17 @@
+function containerWithMostWater(height) {
+  let maxWater = 0;
+  let leftPointer = 0;
+  let rightPointer = height.length - 1;
+
+  while (leftPointer < rightPointer) {
+    maxWater = Math.max(maxWater, Math.min(height[leftPointer], height[rightPointer]) * (height[rightPointer] - 1));
+    if (height[leftPointer] < height[rightPointer]) {
+      leftPointer++;
+    } else {
+      rightPointer--;
+    }
+  }
+  return maxWater;
+}
+
+module.exports = containerWithMostWater;
