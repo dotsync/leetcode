db7fa66947d620a5a3f03cd816bd404b412e6436
Sergei Liddell <liddell.s@pm.me>
2021-03-30 18:07:09 -0600

    Reformats test suite

--- PowerWeekOne/Day01-StringsAndArrays/day01.test.js
+++ PowerWeekOne/Day01-StringsAndArrays/day01.test.js
@@ -1,11 +1,30 @@
 const containerWithMostWater = require('./containerWithMostWater');
 
-describe('containerWithMostWater', () => {
-  test('should return 0 when given an empty input', () => {
-    expect(containerWithMostWater([])).toBe(0);
+describe('Day01 Strings and Arrays Section', () => {
+  /********************/
+  // Easy Problems
+  /********************/
+  describe('Easy Problems', () => {
+
   })
-  test('should return the amount of the most possible water', () => {
-    expect(containerWithMostWater([1, 8, 6, 2, 5, 4, 8, 3, 7])).toBe(49);
-    expect(containerWithMostWater([1, 1])).toBe(1);
+  /********************/
+  // Medium Problems
+  /********************/
+  describe('Medium Problems', () => {
+    describe('containerWithMostWater', () => {
+      test('should return 0 when given an empty input', () => {
+        expect(containerWithMostWater([])).toBe(0);
+      })
+      test('should return the amount of the most possible water', () => {
+        expect(containerWithMostWater([1, 8, 6, 2, 5, 4, 8, 3, 7])).toBe(49);
+        expect(containerWithMostWater([1, 1])).toBe(1);
+      })
+    })
+  })
+  /********************/
+  // Hard Problems
+  /********************/
+  describe('Hard Problems', () => {
+
   })
 })
