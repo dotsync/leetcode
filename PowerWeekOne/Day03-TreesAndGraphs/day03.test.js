668e7c1f0822f5725c5c33301c7c4c124441cc91
Sergei Liddell <liddell.s@pm.me>
2021-03-31 08:46:13 -0600

    Creates test for floodFill problem

--- PowerWeekOne/Day03-TreesAndGraphs/day03.test.js
+++ PowerWeekOne/Day03-TreesAndGraphs/day03.test.js
@@ -1,9 +1,18 @@
+const floodFill = require('./floodFill');
+
 describe('Day03 Trees And Graphs Section', () => {
   /** ******************/
   // Easy Problems
   // floodFill
   /** ******************/
   describe('Easy Problems', () => {
+    describe('floodFill problem', () => {
+      test('should change the colors of pixels', () => {
+        const image1 = [[1, 1, 1], [1, 1, 0], [1, 0, 1]];
+        const expected1 = [[2, 2, 2], [2, 2, 0], [2, 0, 1]];
+        expect(floodFill(image1, 1, 1, 2)).toStrictEqual(expected1);
+      });
+    });
   });
   /** ******************/
   // Medium Problems
---
+++ PowerWeekOne/Day03-TreesAndGraphs/floodFill.js
@@ -0,0 +1,5 @@
+const floodFill = function(image, sr, sc, newColor) {
+
+};
+
+module.exports = floodFill;
