bde597367c8a227f94cf507493450cc649fa9828
Sergei Liddell <liddell.s@pm.me>
2021-03-30 21:25:30 -0600

    Adds semicolons

--- PowerWeekOne/Day02-LinkedList/reverseLinkedList.js
+++ PowerWeekOne/Day02-LinkedList/reverseLinkedList.js
@@ -1,13 +1,13 @@
-const reverseLinkedList = head => {
-  let prev = null
-  let current = head
+const reverseLinkedList = (head) => {
+  let prev = null;
+  let current = head;
   while (current !== null) {
-    let next = current.next
-    current.next = prev
-    prev = current
-    current = next
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
   }
-  return prev
-}
+  return prev;
+};
 
 module.exports = reverseLinkedList;
