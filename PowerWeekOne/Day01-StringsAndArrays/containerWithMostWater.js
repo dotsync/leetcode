09ba70d898fd74ecd4b1746df32f127f0d4f64a9
Sergei Liddell <liddell.s@pm.me>
2021-03-30 18:09:55 -0600

    Removes comments

--- PowerWeekOne/Day01-StringsAndArrays/containerWithMostWater.js
+++ PowerWeekOne/Day01-StringsAndArrays/containerWithMostWater.js
@@ -4,13 +4,6 @@ function containerWithMostWater(height) {
   let rightPointer = height.length - 1;
 
   while (leftPointer < rightPointer) {
-    // maxWater = Math.max(maxWater, Math.min(height[leftPointer], height[rightPointer]) * (height[rightPointer] - 1));
-    // if (height[leftPointer] < height[rightPointer]) {
-    //   leftPointer++;
-    // } else {
-    //   rightPointer--;
-    // }
-
     let min = Math.min(height[leftPointer], height[rightPointer])
     maxWater = Math.max(maxWater, min * (rightPointer - leftPointer))
     if (height[leftPointer] < height[rightPointer]) {
