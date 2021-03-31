3eaff0e1bce3ee7b58f9d741fba4ca860b6d5535
Sergei Liddell <liddell.s@pm.me>
2021-03-30 21:32:47 -0600

    Plans test suite for day03

---
+++ PowerWeekOne/Day03-TreesAndGraphs/day03.test.js
@@ -0,0 +1,23 @@
+describe('Day03 Trees And Graphs Section', () => {
+  /** ******************/
+  // Easy Problems
+  // floodFill
+  /** ******************/
+  describe('Easy Problems', () => {
+  });
+  /** ******************/
+  // Medium Problems
+  // binaryTreeLevelOrderTraversal
+  // validateBinarySearchTree
+  // courseScheduler
+  // numberOfIslands
+  /** ******************/
+  describe('Medium Problems', () => {
+  });
+  /** ******************/
+  // Hard Problems
+  // wordLadder
+  /** ******************/
+  describe('Hard Problems', () => {
+  });
+});
