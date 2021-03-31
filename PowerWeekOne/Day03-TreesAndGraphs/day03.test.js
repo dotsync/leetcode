89df2b410616758049140ecdcc6e13d8e2aedfad
Sergei Liddell <liddell.s@pm.me>
2021-03-31 17:32:15 -0600

    Implements test suite for Binary Tree class

--- PowerWeekOne/Day03-TreesAndGraphs/day03.test.js
+++ PowerWeekOne/Day03-TreesAndGraphs/day03.test.js
@@ -1,7 +1,44 @@
+const BinaryTree = require('./BinaryTree');
 const floodFill = require('./floodFill');
 const binaryTreePaths = require('./binaryTreePaths');
 
 describe('Day03 Trees And Graphs Section', () => {
+  describe('Binary Tree Class', () => {
+    describe('Binary Tree Class Methods', () => {
+      describe('#insert', () => {
+        test('should insert node as root into empty tree', () => {
+          const bt = new BinaryTree;
+          bt.insert(1);
+          expect(bt.root.data).toBe(1);
+          expect(bt.root.left).toBeNull();
+          expect(bt.root.right).toBeNull();
+        });
+        test('should add left node first', () => {
+          const bt = new BinaryTree;
+          bt.insert(1);
+          bt.insert(444);
+          expect(bt.root.data).toBe(1);
+          expect(bt.root.left).toBe(444);
+          expect(bt.root.left.left).toBeNull();
+          expect(bt.root.left.right).toBeNull();
+          expect(bt.root.right).toBeNull();
+        });
+        test('should add right node second', () => {
+          const bt = new BinaryTree;
+          bt.insert(5);
+          bt.insert(9);
+          bt.insert(3);
+          expect(bt.root.data).toBe(5);
+          expect(bt.root.left.data).toBe(9);
+          expect(bt.root.left.left).toBeNull();
+          expect(bt.root.left.right).toBeNull();
+          expect(bt.root.right.data).tobe(3);
+          expect(bt.root.right.left).toBeNull();
+          expect(bt.root.right.right).toBeNull();
+        });
+      });
+    });
+  });
   /** ******************/
   // Easy Problems
   // binaryTreePaths
@@ -20,7 +57,15 @@ describe('Day03 Trees And Graphs Section', () => {
       });
     });
     describe('binaryTreePaths', () => {
+      test('should return a list of all possible routes in tree', () => {
 
+      });
+      test('should work when there is only one node in the tree', () => {
+
+      });
+      test('should return empty array if given an empty tree', () => {
+
+      });
     });
   });
   /** ******************/
