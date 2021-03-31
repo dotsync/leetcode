521d9227c63e62fec8293073c005b31d9b5fd317
Sergei Liddell <liddell.s@pm.me>
2021-03-30 18:44:52 -0600

    Enforces google style guide

--- PowerWeekOne/Day02-LinkedList/day02.test.js
+++ PowerWeekOne/Day02-LinkedList/day02.test.js
@@ -1,67 +1,72 @@
-const SinglyLinkedList = require('../../LinkedLists/SinglyLinkedList/SinglyLinkedList');
-const copyListWithRandomPointer = require('./copyListWithRandomPointer');
+const SinglyLinkedList = require(
+    '../../LinkedLists/SinglyLinkedList/SinglyLinkedList');
+// const copyListWithRandomPointer = require('./copyListWithRandomPointer');
 const reverseLinkedList = require('./reverseLinkedList');
 const mergeTwoSortedLists = require('./mergeTwoSortedLists');
 
 describe('Day02 LinkedList List Section', () => {
-  /********************/
+  /** ******************/
   // Easy Problems
-  /********************/
+  /** ******************/
   describe('Easy Problems', () => {
     describe('#mergeTwoSortedLists', () => {
       test('should work when given two empty lists', () => {
-        const list1 = new SinglyLinkedList
-        const list2 = new SinglyLinkedList
+        const list1 = new SinglyLinkedList;
+        const list2 = new SinglyLinkedList;
         expect(mergeTwoSortedLists(list1.head, list2.head)).toBeNull();
-      })
+      });
       test('should merge when list 1 is empty and 2 is full', () => {
-        const list1 = new SinglyLinkedList
+        const list1 = new SinglyLinkedList;
         const list2 = SinglyLinkedList.createListFromValues(2, 3, 5);
         const expected = SinglyLinkedList.createListFromValues(2, 3, 5);
-        expect(mergeTwoSortedLists(list1.head, list2.head)).toStrictEqual(expected.head);
-      })
+        const actual = mergeTwoSortedLists(list1.head, list2.head);
+        expect(actual).toStrictEqual(expected.head);
+      });
       test('should merge when list 2 is empty and 1 is full', () => {
         const list1 = SinglyLinkedList.createListFromValues(1, 4, 6);
-        const list2 = new SinglyLinkedList
+        const list2 = new SinglyLinkedList;
         const expected = SinglyLinkedList.createListFromValues(1, 4, 6);
-        expect(mergeTwoSortedLists(list1.head, list2.head)).toStrictEqual(expected.head);
-      })
+        const actual = mergeTwoSortedLists(list1.head, list2.head);
+        expect(actual).toStrictEqual(expected.head);
+      });
       test('should merge the two sorted lists', () => {
         const list1 = SinglyLinkedList.createListFromValues(1, 4, 6);
         const list2 = SinglyLinkedList.createListFromValues(2, 3, 5);
-        const expected = SinglyLinkedList.createListFromValues(1, 2, 3, 4, 5, 6);
-        expect(mergeTwoSortedLists(list1.head, list2.head)).toStrictEqual(expected.head);
-      })
-    })
+        const expected = SinglyLinkedList.createListFromValues(
+            1, 2, 3, 4, 5, 6);
+        const actual = mergeTwoSortedLists(list1.head, list2.head);
+        expect(actual).toStrictEqual(expected.head);
+      });
+    });
     describe('#reverseLinkedList', () => {
       test('should work with an empty list', () => {
-        const ll = new SinglyLinkedList
+        const ll = new SinglyLinkedList;
         expect(reverseLinkedList(ll.head)).toBeNull();
-      })
+      });
       test('should reverse the given list', () => {
-        const input = SinglyLinkedList.createListFromValues(5, 4, 3, 2, 1)
-        const expected = SinglyLinkedList.createListFromValues(1, 2, 3, 4, 5)
-        expect(reverseLinkedList(input.head)).toStrictEqual(expected.head)
-      })
-    })
-  })
-  /********************/
+        const input = SinglyLinkedList.createListFromValues(5, 4, 3, 2, 1);
+        const expected = SinglyLinkedList.createListFromValues(1, 2, 3, 4, 5);
+        expect(reverseLinkedList(input.head)).toStrictEqual(expected.head);
+      });
+    });
+  });
+  /** ******************/
   // Medium Problems
-  /********************/
+  /** ******************/
   describe('Medium Problems', () => {
     describe('copyListWithRandomPointer', () => {
-      //TODO: figure out how to test this
-    })
+      // TODO: figure out how to test this
+    });
     describe('addingTwoNumbers', () => {
-    })
-  })
-  /********************/
+    });
+  });
+  /** ******************/
   // Hard Problems
-  /********************/
+  /** ******************/
   describe('Hard Problems', () => {
     describe('reverseNodesInKGroups', () => {
-    })
+    });
     describe('mergeKSortedLists', () => {
-    })
-  })
-})
+    });
+  });
+});
