b50ea993aff8434eada6bca5ed6754a7eb5d0888
Sergei Liddell <liddell.s@pm.me>
2021-03-30 18:40:16 -0600

    Cleans up code to make more readable

--- PowerWeekOne/Day02-LinkedList/copyListWithRandomPointer.js
+++ PowerWeekOne/Day02-LinkedList/copyListWithRandomPointer.js
@@ -7,16 +7,17 @@ const copyListWithRandomPointer = (head) => {
    */
   function copy(node) {
     if (!node) return null;
+    // If exists in map we dont need to build
     if (map.get(node)) return map.get(node);
+    // create clone and insert into map
+    const clone = {data: node.data};
+    map.set(node, clone);
 
-    const n = {data: node.data};
-    map.set(node, n);
-
-    n.next = copy(node.next);
-    n.random = copy(node.random);
-    return n;
+    // copy clones properties recursivly
+    clone.next = copy(node.next);
+    clone.random = copy(node.random);
+    return clone;
   }
-
   return copy(head);
 };
 
