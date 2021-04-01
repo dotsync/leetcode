b90dc4c68b351e823c9073b31f3d7182b8d49abb
Sergei Liddell <liddell.s@pm.me>
2021-03-31 18:22:14 -0600

    Solves binaryTreePaths problem

--- PowerWeekOne/Day03-TreesAndGraphs/binaryTreePaths.js
+++ PowerWeekOne/Day03-TreesAndGraphs/binaryTreePaths.js
@@ -1,5 +1,29 @@
-const binaryTreePaths = () => {
+const binaryTreePaths = (root) => {
+  const paths = [];
+  if (root === null) return paths;
+  dfsPrint(root, '', paths);
+  return paths;
 
+  /**
+   * @param {Node} currentNode
+   * @param {String} currentPath string to add currentNode to
+   * @param {Array} paths final result list to push string to
+   */
+  function dfsPrint(currentNode, currentPath, paths) {
+    // add current node data to string
+    currentPath += currentNode.data;
+    // if currentNode is a leaf node then push string to result and return
+    if (currentNode.left === null && currentNode.right === null) {
+      paths.push(currentPath);
+      return;
+    }
+    if (currentNode.left !== null) {
+      dfsPrint(currentNode.left, currentPath + '->', paths);
+    }
+    if (currentNode.right !== null) {
+      dfsPrint(currentNode.right, currentPath + '->', paths);
+    }
+  }
 };
 
 module.exports = binaryTreePaths;
