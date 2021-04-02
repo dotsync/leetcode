23ea64538db662210a02e3588b82ba6d11b50291
Sergei Liddell <liddell.s@pm.me>
2021-04-01 21:29:07 -0600

    Adds comments and time/space analysis

--- MostCommonQuestions/Recursion/generateParenthesis.js
+++ MostCommonQuestions/Recursion/generateParenthesis.js
@@ -5,20 +5,25 @@ const generateParenthesis = (n) => {
   return combinations;
 
   /**
-   * @param {array} result
-   * @param {string} currentString
-   * @param {int} openCounter
-   * @param {int} closeCounter
-   * @param {int} max
+   * @param {array} result An array to push final string to
+   * @param {string} currentString the current valid parenthesis being generated
+   * @param {int} openCounter counts open parenthesis
+   * @param {int} closeCounter counts open parenthesis
+   * @param {int} max max amount of each left and right
    */
   function generateString(result, currentString, openCounter, closeCounter, max) {
+    // If string is finished, push to result
     if (currentString.length === max * 2) {
       result.push(currentString);
       return;
     }
+    // generate left is possible this time
     if (openCounter < max) generateString(result, currentString + '(', openCounter + 1, closeCounter, max);
+    // generate right if possile this time
     if (closeCounter < openCounter) generateString(result, currentString + ')', openCounter, closeCounter + 1, max);
   }
 };
 
 module.exports = generateParenthesis;
+
+// time and space is O(4^n / sqrt(n))
