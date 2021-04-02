41cef600a7ed8098d434c9979b05518c02911cc8
Sergei Liddell <liddell.s@pm.me>
2021-04-01 21:22:53 -0600

    Solves generateParenthesis problem

--- MostCommonQuestions/Recursion/generateParenthesis.js
+++ MostCommonQuestions/Recursion/generateParenthesis.js
@@ -1,16 +1,24 @@
-/**
- * @param {Int} n pairs of parenthesis
- * @return {Array} list of all combinations of well-formed parentheses.
- */
-function generateParenthesis(n) {
+/* eslint-disable max-len */
+const generateParenthesis = (n) => {
   const combinations = [];
+  generateString(combinations, '', 0, 0, n);
+  return combinations;
 
-  if (n == 0) return combinations;
-  generateParenthesis(n-1);
-
-  function pair(n, string, ) {
-
+  /**
+   * @param {array} result
+   * @param {string} currentString
+   * @param {int} openCounter
+   * @param {int} closeCounter
+   * @param {int} max
+   */
+  function generateString(result, currentString, openCounter, closeCounter, max) {
+    if (currentString.length === max * 2) {
+      result.push(currentString);
+      return;
+    }
+    if (openCounter < max) generateString(result, currentString + '(', openCounter + 1, closeCounter, max);
+    if (closeCounter < openCounter) generateString(result, currentString + ')', openCounter, closeCounter + 1, max);
   }
-}
+};
 
 module.exports = generateParenthesis;
