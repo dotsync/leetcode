45e8b109707091c06291b2efb0c665cbb408a6b1
Sergei Liddell <liddell.s@pm.me>
2021-04-01 18:18:26 -0600

    Creates test suite for fibonacci

--- PowerWeekOne/Day04-Recursion/day04.test.js
+++ PowerWeekOne/Day04-Recursion/day04.test.js
@@ -1,8 +1,22 @@
+const fibonacci = require('./fibonacci');
+
 describe('Day04 Recursion Section', () => {
   /** ******************/
   // Easy Problems
   /** ******************/
   describe('Easy Problems', () => {
+    describe('Fibonacci Problem', () => {
+      test('should return 0 if input is 0', () => {
+        expect(fibonacci(0)).toBe(0);
+      });
+      test('should return 1 if input is 1', () => {
+        expect(fibonacci(1)).toBe(1);
+      });
+      test('should return the fibonacci of the given n', () => {
+        expect(fibonacci(4)).toBe(24);
+        expect(fibonacci(5)).toBe(120);
+      });
+    });
   });
   /** ******************/
   // Medium Problems
