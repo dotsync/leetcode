803b367a50ba5663959fc8eb0cc0774307d2ca34
Sergei Liddell <liddell.s@pm.me>
2021-04-04 14:20:58 -0600

    Creates test suite for Queue #enqueue method

--- Queue/Queue.test.js
+++ Queue/Queue.test.js
@@ -6,4 +6,29 @@ describe('Queue', () => {
     expect(queue.queue.head).toBeNull();
     expect(queue.size).toBe(0);
   });
+  describe('Queue Methods', () => {
+    describe('#enqueue', () => {
+      const queue = new Queue;
+      // add one, then two then three
+      // head 1 2 3 tail
+      test('should add data to an empty list', () => {
+        queue.enqueue(1);
+        expect(queue.size).toBe(1);
+        expect(queue.queue.head.data).toBe(1);
+      })
+      test('should add data to a list containing just one item', () => {
+
+      })
+      test('should add data to a very large list', () => {
+
+      })
+
+    });
+    describe('#dequeue', () => {
+
+    });
+    describe('#peek', () => {
+
+    });
+  });
 });
