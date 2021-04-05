ee207dd829ac32bb5c97dc79716ae6ad162dd46b
Sergei Liddell <liddell.s@pm.me>
2021-04-05 16:51:49 -0600

    Solves firstUniqueChar Problem

--- MostCommonQuestions/StringsAndArrays/firstUniqueChar.js
+++ MostCommonQuestions/StringsAndArrays/firstUniqueChar.js
@@ -1,6 +1,24 @@
 const firstUniqueChar = (string) => {
-
+  // create hashmap
+  const uniques = {};
+  for (const char in string) {
+    // if we haven't seen letter before, put it in hash with count
+    if (!uniques[string[char]]) {
+      uniques[string[char]] = 1;
+    } else {
+      // else we have seen letter before, increase count
+      uniques[string[char]] ++;
+    }
+  }
+  // iterate string a second time
+  for (const char in string) {
+    // compare current character in string to hash
+    if (uniques[string[char]] == 1) {
+      return Number(char);
+    }
+  };
+  // If nothing returned from the above loop, nothing was found
+  return -1;
 };
 
 module.exports = firstUniqueChar;
-
