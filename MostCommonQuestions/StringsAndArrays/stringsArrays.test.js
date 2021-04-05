15e2db9c60859c0665924123ddf423cbcf4cf5c8
Sergei Liddell <liddell.s@pm.me>
2021-04-05 16:53:22 -0600

    Implements test suite for firstUniqChar

--- MostCommonQuestions/StringsAndArrays/stringsArrays.test.js
+++ MostCommonQuestions/StringsAndArrays/stringsArrays.test.js
@@ -2,21 +2,21 @@ const containerWithMostWater = require('./containerWithMostWater');
 const firstUniqueChar = require('./firstUniqueChar');
 const threeSum = require('./threeSum');
 
-describe('Day01 Strings and Arrays Section', () => {
+describe('Strings and Arrays Section', () => {
   /** ******************/
   // Easy Problems
   /** ******************/
   describe('Easy Problems', () => {
-    // describe('firstUniqueChar', () => {
-    //   test('should return index of first non repeating char found', () => {
-    //     expect(firstUniqueChar('leetcode')).toBe(0);
-    //     expect(firstUniqueChar('loveleetcode')).toBe(2);
-    //   });
-    //   test('should return -1 if there are no repeating characters', () => {
-    //     expect(firstUniqueChar('')).toBe(-1);
-    //     expect(firstUniqueChar('aabb')).toBe(-1);
-    //   });
-    // });
+    describe('firstUniqueChar', () => {
+      test('should return index of first non repeating char found', () => {
+        expect(firstUniqueChar('leetcode')).toBe(0);
+        expect(firstUniqueChar('loveleetcode')).toBe(2);
+      });
+      test('should return -1 if there are no repeating characters', () => {
+        expect(firstUniqueChar('')).toBe(-1);
+        expect(firstUniqueChar('aabb')).toBe(-1);
+      });
+    });
   });
   /** ******************/
   // Medium Problems
