89c4e01f3db6d165380c3c278740406e21c1c7f0
Sergei Liddell <liddell.s@pm.me>
2021-04-07 22:08:58 -0600

    Solve valid validParenthesis

---
+++ MostCommonQuestions/StringsAndArrays/validParenthesis.js
@@ -0,0 +1,32 @@
+/* eslint-disable require-jsdoc */
+const validParenthesis = (string) => {
+  const arr = string.split('');
+  // check length, if odd return false because we need pairs
+  if (string.length % 2 !== 0) return false;
+  // declare right = halfway point, left = right -1
+  let right = Math.floor(string.length / 2); let left = right - 1;
+  // iterate with while loop (while right is < s.length)
+  while (left > 0) {
+    // if !isPair(right, left)
+    if (!isPair(arr, left, right)) {
+      return false;
+    } else {
+      // else move iterators to check next pair
+      right ++;
+      left --;
+    }
+  }
+  return true;
+  // helper checks i vs j
+  function isPair(array, a, b) {
+    console.log(array[a], array[b])
+    if (array[a] === '(' && array[b] === ')') return true;
+    // if (array[a] === ')' && array[b] === '(') return true;
+    else if (array[a] === '[' && array[b] === ']') return true;
+    // if (array[a] === ']' && array[b] === '[') return true;
+    else if (array[a] === '{' && array[b] === '}') return true;
+    // if (array[a] === '}' && array[b] === '{') return true;
+  }
+};
+
+module.exports = validParenthesis;
