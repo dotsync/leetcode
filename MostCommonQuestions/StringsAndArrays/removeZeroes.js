66d1b6f8ccf998f25b0bec70a2a307ad3c0a92d1
Sergei Liddell <liddell.s@pm.me>
2021-04-10 18:59:32 -0600

    Solves removeZeroes problem

--- MostCommonQuestions/StringsAndArrays/removeZeroes.js
+++ MostCommonQuestions/StringsAndArrays/removeZeroes.js
@@ -1,5 +1,18 @@
 const removeZeroes = (arr) => {
-
+  // iterate arr
+  // move all chars
+  let j = 0;
+  for (let i = 0; i < arr.length; i ++) {
+    // if the current is not a zero, swap i
+    if (arr[i] !== 0) {
+      arr[j++] = arr[i];
+    }
+  }
+  // add on extra zeros
+  for (let i = j; i < arr.length; i ++) {
+    arr[i] = 0;
+  }
+  return arr;
 };
 
 // Don't use extra space
