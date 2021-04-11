08c0aa46b86d022f1ee77269012d63721c2d414b
Sergei Liddell <liddell.s@pm.me>
2021-04-10 19:39:32 -0600

    Adds edge case test for removeZeros problem

--- MostCommonQuestions/StringsAndArrays/stringsArrays.test.js
+++ MostCommonQuestions/StringsAndArrays/stringsArrays.test.js
@@ -68,6 +68,9 @@ describe('Strings and Arrays Section', () => {
         expect(removeZeroes([0, 1, 0, 3, 12])).toStrictEqual([1, 3, 12, 0, 0]);
         expect(removeZeroes([0])).toStrictEqual([0]);
       });
+      test('should find no zeroes', () => {
+        expect(removeZeroes([1, 2, 3])).toStrictEqual([1, 2, 3]);
+      });
     });
   });
   /** ******************/
