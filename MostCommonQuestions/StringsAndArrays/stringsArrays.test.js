786778eca56e6eb3f6de7b36cdaf728fe35631f7
Sergei Liddell <liddell.s@pm.me>
2021-04-10 18:52:43 -0600

    Creates failing test for removeZeroes problem

--- MostCommonQuestions/StringsAndArrays/stringsArrays.test.js
+++ MostCommonQuestions/StringsAndArrays/stringsArrays.test.js
@@ -66,6 +66,7 @@ describe('Strings and Arrays Section', () => {
     describe('removeZeroes', () => {
       test('should move all zeroes to the end of the array and keep origional order', () => {
         expect(removeZeroes([0, 1, 0, 3, 12])).toStrictEqual([1, 3, 12, 0, 0]);
+        expect(removeZeroes([0])).toStrictEqual([0]);
       });
     });
   });
