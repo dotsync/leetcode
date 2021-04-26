c4bd17d5223bad2f84e4417f37a50bad2711b624
Sergei Liddell <liddell.s@pm.me>
2021-04-25 18:49:30 -0600

    Implements fizz buz in c++

--- C++/fizzbuzz.cpp
+++ C++/fizzbuzz.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
 int main() {
+  int max;
+  std::cout << "What is your number: ";
+  std::cin >> max;
 
-  for (int i = 1 ; i <= 100; i ++) {
+
+  for (int i = 1 ; i <= max; i ++) {
     if (i % 3 == 0 && i % 5 != 0) {
       std::cout << "Fizz\n";
     } else if (i % 3 != 0 && i % 5 == 0) {
