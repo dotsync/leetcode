5eb4ca8e8266415722ad9b9ea54be03bc0f1087e
Sergei Liddell <liddell.s@pm.me>
2021-05-08 21:02:47 -0600

    Solves removeDuplicates problem

--- patterns/2 pointers/removeDuplicates.js
+++ patterns/2 pointers/removeDuplicates.js
@@ -1,8 +1,15 @@
 const removeDuplicates = (arr) => {
-
+  let replacer = 1;
+  for (let checker = 0; checker < arr.length; checker ++) {
+    if (arr[replacer - 1] !== arr[checker]) {
+      arr[replacer] = arr[checker];
+      replacer ++;
+    }
+  }
+  return replacer;
 };
 
-// time:
-// space:
+// time: O(n)
+// space: O(1)
 
 module.exports = removeDuplicates;
