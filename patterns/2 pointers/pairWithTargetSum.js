2fdda44b2a7e07d9326e7ba5edc8c4776049ab4d
Sergei Liddell <liddell.s@pm.me>
2021-05-08 20:10:48 -0600

    Solves pairWithTargetSum problem

--- patterns/2 pointers/pairWithTargetSum.js
+++ patterns/2 pointers/pairWithTargetSum.js
@@ -1,5 +1,15 @@
 const pairWithTargetSum = (arr, target) => {
   // return -1 if unable to find
+  let left = 0; let right = arr.length-1;
+  while (left < right) {
+    if (arr[right] + arr[left] === target) return [left, right];
+    else if (arr[right] + arr[left] > target) right --;
+    else left ++;
+  }
+  return -1;
 };
+
+// time O(n)
+// space O(1)
 
 module.exports = pairWithTargetSum;
