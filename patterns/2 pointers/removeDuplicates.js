b92218e0db208ef15df7641fafd5935c1b532ddd
Sergei Liddell <liddell.s@pm.me>
2021-05-08 20:39:18 -0600

    Creates test suite for removeDuplicates problem

---
+++ patterns/2 pointers/removeDuplicates.js
@@ -0,0 +1,8 @@
+const removeDuplicates = (arr) => {
+
+};
+
+// time:
+// space:
+
+module.exports = removeDuplicates;
--- patterns/2 pointers/twoPointer.test.js
+++ patterns/2 pointers/twoPointer.test.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 const pairWithTargetSum = require('./pairWithTargetSum');
+const removeDuplicates = require('./removeDuplicates');
 
 describe('twoPointer Pattern', () => {
   describe('pairWithTargetSum', () => {
@@ -16,6 +17,17 @@ describe('twoPointer Pattern', () => {
     });
     test('should return the indeces at which the first compliments are found', () => {
       expect(pairWithTargetSum([1, 4, 5, 6, 7, 8], 9)).toStrictEqual([0, 5]);
+    });
+  });
+  describe('removeDuplicates', () => {
+    test('should return length of origional array if there are no duplicates', () => {
+      expect(removeDuplicates([1, 2, 3])).toBe(3);
+    });
+    test('should retun lenght of modified array without duplicates', () => {
+      expect(removeDuplicates([1, 2, 3, 3])).toBe(3);
+      expect(removeDuplicates([1, 1, 2, 3, 3])).toBe(3);
+      expect(removeDuplicates([1, 1, 1, 3])).toBe(2);
+      expect(removeDuplicates([1, 1, 1, 1])).toBe(1);
     });
   });
 });
