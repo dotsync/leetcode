4ff944dd72312daa147083c20a067700126bf4f8
Sergei Liddell <liddell.s@pm.me>
2021-05-16 15:57:57 -0600

    solves permutationsInAString problem

---
+++ patterns/sliding-window/permutationsInAString.js
@@ -0,0 +1,47 @@
+/* eslint-disable max-len */
+const permutationsInAString = (str, pattern) => {
+  if (pattern.length === 0) return false;
+  let windowStart = 0;
+  let matched = 0;
+  const charFrequency = {};
+
+  for (i = 0; i < pattern.length; i++) {
+    const chr = pattern[i];
+    if (!(chr in charFrequency)) {
+      charFrequency[chr] = 0;
+    }
+    charFrequency[chr] += 1;
+  }
+
+  // Our goal is to match all the characters from the 'charFrequency' with the current window
+  // try to extend the range [windowStart, windowEnd]
+  for (windowEnd = 0; windowEnd < str.length; windowEnd++) {
+    const rightChar = str[windowEnd];
+    if (rightChar in charFrequency) {
+      // Decrement the frequency of matched character
+      charFrequency[rightChar] -= 1;
+      if (charFrequency[rightChar] === 0) {
+        matched += 1;
+      }
+    }
+
+    if (matched === Object.keys(charFrequency).length) {
+      return true;
+    }
+
+    // Shrink the sliding window
+    if (windowEnd >= pattern.length - 1) {
+      leftChar = str[windowStart];
+      windowStart += 1;
+      if (leftChar in charFrequency) {
+        if (charFrequency[leftChar] === 0) {
+          matched -= 1;
+        }
+        charFrequency[leftChar] += 1;
+      }
+    }
+  }
+  return false;
+};
+
+module.exports = permutationsInAString;
