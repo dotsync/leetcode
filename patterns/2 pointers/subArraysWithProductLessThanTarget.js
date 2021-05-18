09e8f10b371ca129c1a0805511be41645704c7b1
Sergei Liddell <liddell.s@pm.me>
2021-05-18 11:29:46 -0600

    Solves subArraysWithProductLessThanTarget

--- patterns/2 pointers/subArraysWithProductLessThanTarget.js
+++ patterns/2 pointers/subArraysWithProductLessThanTarget.js
@@ -1,5 +1,26 @@
-const subArraysWithProductLessThanTarget = () => {
+const Deque = require('collections/deque');
+// http://www.collectionsjs.com
 
+const subArraysWithProductLessThanTarget = (nums, target) => {
+  const result = [];
+  let product = 1;
+  let left = 0;
+  for (let right = 0; right < nums.length; right++) {
+    // if (nums[i] >= target) continue;
+    // push current num
+    // result.push([nums[i]]);
+    product *= nums[right];
+    while (product >= target && left < nums.length) {
+      product /= nums[left];
+      left += 1;
+    }
+    const tempList = new Deque();
+    for (let i = right; i > left - 1; i--) {
+      tempList.unshift(nums[i]);
+      result.push(tempList.toArray());
+    }
+  };
+  return result;
 };
 
 module.exports = subArraysWithProductLessThanTarget;
