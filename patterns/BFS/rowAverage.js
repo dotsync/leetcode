fa4563b62517f519301c41ab92674565f06045b5
Sergei Liddell <liddell.s@pm.me>
2021-05-25 20:40:15 -0600

    Solves rowAverage BFS problem

--- patterns/BFS/rowAverage.js
+++ patterns/BFS/rowAverage.js
@@ -1,6 +1,20 @@
 const rowAverage = (root) => {
   const result = [];
-
+  const queue = [];
+  if (root === null) return result;
+  queue.push(root);
+  let node;
+  while (queue.length) {
+    const rowLength = queue.length;
+    let rowMax = 0;
+    for (let i = 0; i < rowLength; i++) {
+      node = queue.shift();
+      rowMax += node.data;
+      if (node.left !== null) queue.push(node.left);
+      if (node.right !== null) queue.push(node.right);
+    }
+    result.push(rowMax / rowLength);
+  }
   return result;
 };
 
