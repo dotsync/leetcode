37dbea7d2f18f7cef32cfb6fd4dc80024fafe36e
Sergei Liddell <liddell.s@pm.me>
2021-05-25 21:24:55 -0600

    Adds psuedocode and code analysis

--- patterns/BFS/rowAverage.js
+++ patterns/BFS/rowAverage.js
@@ -1,21 +1,30 @@
 const rowAverage = (root) => {
   const result = [];
-  const queue = [];
   if (root === null) return result;
-  queue.push(root);
+  const queue = [];
   let node;
+  // fill queue with first item
+  queue.push(root);
   while (queue.length) {
     const rowLength = queue.length;
     let rowMax = 0;
+    // iterate only the portion of the queue that is this row
     for (let i = 0; i < rowLength; i++) {
+      // pull first item from queue
       node = queue.shift();
+      // increase count of rowMax by the amount of current node on this level
       rowMax += node.data;
+      // check if we need to add left and right to the queue
       if (node.left !== null) queue.push(node.left);
       if (node.right !== null) queue.push(node.right);
     }
+    // now that we have checked this entire row, push average to result
     result.push(rowMax / rowLength);
   }
   return result;
 };
+
+// Time: O(n)
+// Space: O(n)
 
 module.exports = rowAverage;
