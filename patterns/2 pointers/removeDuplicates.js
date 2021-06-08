c8797bc7514ab4a0b6a0d858afeff8d184df5971
Sergei Liddell <liddell.s@pm.me>
2021-06-08 00:02:30 -0600

    Solves removeDuplicates problem

--- patterns/2 pointers/removeDuplicates.js
+++ patterns/2 pointers/removeDuplicates.js
@@ -1,13 +1,74 @@
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-trailing-spaces */
+// [1, 1, 2, 2, 3] -> [1, 2, 3]
 const removeDuplicates = (arr) => {
-  let replacer = 1;
-  for (let checker = 0; checker < arr.length; checker ++) {
-    if (arr[replacer - 1] !== arr[checker]) {
-      arr[replacer] = arr[checker];
-      replacer ++;
+  if (arr.length < 2) return arr;
+  // create pointers
+  let j = 1;
+  // iterate i across the entire arr
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[j - 1]) {
+      arr[j] = arr[i];
+      j++;
     }
   }
-  return replacer;
+  return j;
 };
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+// const removeDuplicates = (arr) => {
+//   let replacer = 1;
+//   for (let checker = 0; checker < arr.length; checker ++) {
+//     if (arr[replacer - 1] !== arr[checker]) {
+//       arr[replacer] = arr[checker];
+//       replacer ++;
+//     }
+//   }
+//   return replacer;
+// };
 
 // time: O(n)
 // space: O(1)
