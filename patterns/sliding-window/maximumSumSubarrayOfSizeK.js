30cb7d48ae237644d8385e95cca280fc6d569394
Sergei Liddell <liddell.s@pm.me>
2021-06-08 21:56:26 -0600

    Solves maximumSumSubarrayOfSizeK

--- patterns/sliding-window/maximumSumSubarrayOfSizeK.js
+++ patterns/sliding-window/maximumSumSubarrayOfSizeK.js
@@ -1,20 +1,37 @@
-const maximumSumSubarrayOfSizeK = (array, k) => {
-  let maxSum = 0;
-  let currentSum = 0;
-  if (k > array.length) return null;
-  // create initial window
-  for (let i = 0; i < k; i++) {
-    maxSum += array[i];
+// const maximumSumSubarrayOfSizeK = (array, k) => {
+//   let maxSum = 0;
+//   let currentSum = 0;
+//   if (k > array.length) return null;
+//   // create initial window
+//   for (let i = 0; i < k; i++) {
+//     maxSum += array[i];
+//   }
+//   // set current sum to be initial window
+//   currentSum = maxSum;
+//   // move window
+//   for (let i = k; i < array.length; i ++) {
+//     currentSum = currentSum - array[i - k] + array[i]
+//     // compute max
+//     maxSum = Math.max(maxSum, currentSum);
+//   }
+//   return maxSum;
+// };
+
+const maximumSumSubarrayOfSizeK = (arr, k) => {
+  let windowSum = 0;
+  let windowStart = 0;
+  let max = 0;
+
+  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    windowSum += arr[windowEnd];
+    if (windowEnd >= k - 1) {
+      // move start
+      max = Math.max(max, windowSum);
+      windowSum -= arr[windowStart];
+      windowStart++;
+    }
   }
-  // set current sum to be initial window
-  currentSum = maxSum;
-  // move window
-  for (let i = k; i < array.length; i ++) {
-    currentSum = currentSum - array[i - k] + array[i]
-    // compute max
-    maxSum = Math.max(maxSum, currentSum);
-  }
-  return maxSum;
+  return max || windowSum;
 };
 
 module.exports = maximumSumSubarrayOfSizeK;
