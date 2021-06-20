04537e6c4f0d626a26daea351d8beb4234ef83aa
Sergei Liddell <liddell.s@pm.me>
2021-06-20 13:28:33 -0600

    Solves fib sequence problem use memoization
    * Bottom Up

--- patterns/Dynamic Programming/fiboncai.js
+++ patterns/Dynamic Programming/fiboncai.js
@@ -1,8 +1,20 @@
-/* eslint-disable require-jsdoc */
-function fib(n) {
-  // returns first n fibs
-  if (n <= 1) return n;
-  return fib(n - 1) + fib(n - 2);
-}
+// recursive approach
+// const fib = (n) => {
+//   if (n <= 1) return n;
+//   return fib(n - 1) + fib(n - 2);
+// };
+
+// dp approach memoization - bottom up
+const fib = (n) => {
+  const map = {
+    0: 0,
+    1: 1,
+    2: 1,
+  };
+  for (let i = 3; i <= n; i++) {
+    map[i] = map[i - 1] + map[i - 2];
+  }
+  return map[n];
+};
 
 module.exports = fib;
