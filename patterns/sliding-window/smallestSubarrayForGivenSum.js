3d105b1d97462653c44cb89f3f5a6de13692191b
Sergei Liddell <liddell.s@pm.me>
2021-06-19 22:24:03 -0600

    Solves smallestSubarrayForGivenSum

--- patterns/sliding-window/smallestSubarrayForGivenSum.js
+++ patterns/sliding-window/smallestSubarrayForGivenSum.js
@@ -1,51 +1,16 @@
 const smallestSubarrayForGivenSum = (arr, s) => {
-  // let smallestWindow = Infinity;
-  // let windowSum = 0;
-  // let windowStart = 0;
-  // for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-  //   windowSum += arr[windowEnd];
-  //   while (windowSum >= s) {
-  //     smallestWindow = Math.min(smallestWindow, windowEnd - windowStart + 1);
-  //     windowSum -= arr[windowStart];
-  //     windowStart ++;
-  //   }
-  // }
-  // return smallestWindow === Infinity ? 0 : smallestWindow;
-  /**
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   * attempt 2
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   *
-   */
+  let smallestWindow = Infinity;
+  let windowSum = 0;
+  let windowStart = 0;
+  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    windowSum += arr[windowEnd];
+    while (windowSum >= s) {
+      smallestWindow = Math.min(smallestWindow, windowEnd - windowStart + 1);
+      windowSum -= arr[windowStart];
+      windowStart++;
+    }
+  }
+  return smallestWindow === Infinity ? 0 : smallestWindow;
 };
 
 module.exports = smallestSubarrayForGivenSum;
